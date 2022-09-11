24710a12310f1c1262c7082c430e632d31e0de8e
Sreekesh Iyer <67017933+sreekeshiyer@users.noreply.github.com>
2022-09-11 13:31:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return jsonify({
-        'greeting': 'Hello World!'
+        "message": "Hello World."
     })
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug = True)
+    app.run()
